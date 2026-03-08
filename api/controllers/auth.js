847dbfa51f7f5f3f61b15352843d6fae05fc7d69
MingyiQiu <mingyiqiu@foxmail.com>
2026-03-08 15:15:33 -0700

    Add login feature

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //check if user exists
@@ -30,6 +31,34 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (res, req) => {};
+export const login = (req, res) => {
+  const q = "SELECT * FROM users WHERE username = ?";
 
-export const logout = (res, req) => {};
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    //check password
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password,
+    );
+
+    if (!checkPassword)
+      return res.status(400).json("Wrong username or password!");
+
+    const token = jwt.sign({ id: data[0].id }, "secretkey");
+
+    const { password, ...others } = data[0];
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  });
+};
+
+export const logout = (res, req) => {
+  res.clearCookie("accessToken").status(200).json("User has been logged out!");
+};
